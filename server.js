53190b67b756867eced2e15ee1532fd4a39865b9
MKDimensionalTech <mkdimensionaltech@gmail.com>
2022-06-16 12:39:28 -0300

    NODE+EJS

--- server.js
+++ server.js
@@ -5,12 +5,42 @@ const app = express();
 app.set("view engine", "ejs");
 
 app.get("/", function(req, res){
-    res.render("index")
+    const items = [
+        {
+            title: "D",
+            message: "esenvolvedor",
+        },
+        {
+            title: "E",
+            message: "JS",
+        },
+        {
+            title: "M",
+            message: "ais legal",
+        },
+        {
+            title: "A",
+            message: "moroso",
+        },
+        {
+            title: "I",
+            message: "nstall ejs",
+        },
+        {
+            title:"S",
+            message: "intaxe simples"
+        },
+    ]
+    const subtitle = "Uma linguage de modelagem para criação de paginas html para criação de javascript"
+    res.render("pages/index", {
+        qualidades: items,
+        subtitle: subtitle
+    });
 })
 
 app.get("/sobre", function(req, res){
-    res.render("about")
+    res.render("pages/about")
 })
 
 app.listen(8080);
-console.log("Rodando")
+console.log("Servidor funcionando")
